c247147b6c8c1d0f3be8fd6e2a178a5f1bb05e1f
baselatef25 <baselatef25@gmail.com>
2022-01-24 12:01:03 +0200

    fcommit

---
+++ AUT_AL/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+
+# Create your models here.
+#salam page models**************************************************
+class AUTIP (models.Model):
+    title = models.CharField(max_length=50,blank=True, null=True)
+    subtitle = models.CharField(max_length=50,blank=True, null=True)
+    text = models.CharField(max_length=2000,blank=True, null=True)
+    def __str__(self):
+        return self.title
+#Portfolio Modals
+class Portfolio_Modals (models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True)
+    text = models.CharField(max_length=5000,blank=True)
+
+
+
+class Registration(models.Model):
+    fname = models.CharField(max_length=50,blank=True, null=True)
+    lname = models.CharField(max_length=50,blank=True, null=True)
+    email = models.CharField(max_length=120,blank=True, null=True)
+    phoneno = models.CharField(max_length=20,blank=True, null=True)
+    pirthday = models.DateField(blank=True, null=True)
+    address = models.CharField(max_length=200,blank=True, null=True)
+    level = models.CharField(max_length=200,blank=True, null=True)
+
+class blog(models.Model):
+    title = models.CharField(max_length=100)
+    pragrapg1 = models.CharField(max_length=5000,blank=True, null=True)
+    pragrapg2 = models.CharField(max_length=5000,blank=True, null=True)
+    pragrapg4 = models.CharField(max_length=5000,blank=True, null=True)
+    pragrapg5 = models.CharField(max_length=5000,blank=True, null=True)
+    pragrapg6 = models.CharField(max_length=5000,blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True, null=True)
+
+class why(models.Model):
+    title = models.CharField(max_length=100,blank=True, null=True)
+    pragrapg1 = models.CharField(max_length=5000,blank=True)
+    pragrapg2 = models.CharField(max_length=5000,blank=True)
+    image = models.ImageField(blank=True, null=True)
+
+class homeslids(models.Model):
+    title = models.CharField(max_length=100,blank=True, null=True)
+    pragrapg = models.TextField(max_length=5000,blank=True)
+    link = models.CharField(max_length=1000,blank=True)
+    image = models.ImageField(blank=True, null=True)
+
+class news(models.Model):
+    title = models.CharField(max_length=1500,blank=True, null=True)
+    pragrapg = models.CharField(max_length=5000,blank=True)
+    day = models.CharField(max_length=5,blank=True)
+    month = models.CharField(max_length=5,blank=True)
+    link = models.CharField(max_length=1000,blank=True)
+    image = models.ImageField(blank=True, null=True)
+
+class faculty_members (models.Model):
+    name = models.CharField(max_length=1000,blank=True)
+    jop= models.CharField(max_length=1000,blank=True)
+    image = models.ImageField(blank=True, null=True)
+    degree= models.CharField(max_length=1000,blank=True)
+    facebook = models.CharField(max_length=1500,blank=True)
+    twitter = models.CharField(max_length=1500,blank=True)
+    linkedin = models.CharField(max_length=1500,blank=True)
+
+
+class images (models.Model):
+    image = models.ImageField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=500,blank=True)
+    text = models.CharField(max_length=5000,blank=True)
+
+class aboutprogram (models.Model):
+    name = models.CharField(max_length=5000,blank=True)
---
+++ AUT_AL/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import path
+from AUT_AL.views import *
+from AUT_AL import views 
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$',home),
+    url(r'^about/$',About),
+    url(r'^Salam_Program/$',AUT_Intercultural_Program),
+    url(r'^apply/$',Apply),
+    url(r'^blog/$',Blog),
+    url(r'^whySalam_Program/$',whyautip),
+    url(r'^123/dashbord/$',dashbord),
+    url(r'^123/$',dashbord_login),
+    url(r'^news/$',newsv),
+    url(r'^academic/$',academic),
+    url(r'^123/dashbord/dview_orders/$',dvieworders),
+    url(r'^123/dashbord/dhome/$',dhome),
+    url(r'^123/dashbord/dSalamProgram/$',dSalamProgram),
+    url(r'^delete/(?P<id>\d+)/$', deleteslid , name="delete_slide"),
+    #url(r'^delete/(?P<pk>\d+)/$', 'note.views.NoteDelete.as_view()', name="delete_note"),
+
+
+]
+
